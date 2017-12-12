f0663899f70be25884550eb73c59a50481d72030
ElPopcorn <sonza.victor@gmail.com>
2017-12-12 13:58:12 +0100

    "modification of Carnivore"

--- src/com/vivarium/model/Carnivore.java
+++ src/com/vivarium/model/Carnivore.java
@@ -1,12 +1,20 @@
-include Herbivore;
-include Animal
-class Carnivore extends Animal
+package com.vivarium.model;
+
+abstract class Carnivore extends Animal
 {
+
 	public Carnivore(int posX,int posY,float health, float hunger, float vitality,float speed, Sex type)
 	{
 		super(posX,posY,health,hunger,vitality,speed,type);
 	}
 
-	public boolean eat(Herbivore other);
-	public void evoluate(float deltaTime);
+	boolean eat(Herbivore other)
+	{
+		if(this.speed > other.speed)
+		{
+			other.setHP(0);
+			return  true;
+		}
+	}
+	abstract public void evoluate(float deltaTime);
 }
