edbf65fdbfb45bb6beaac9cc9508c234bfa9d69b
Piryus <piryus@gmail.com>
2017-12-15 15:06:16 +0100

    Add option to spawn organisms

--- src/com/vivarium/controller/SidePanelActionListener.java
+++ src/com/vivarium/controller/SidePanelActionListener.java
@@ -1,17 +1,20 @@
 package com.vivarium.controller;
 
 import com.vivarium.model.Sex;
-import com.vivarium.model.Wolf;
+import com.vivarium.model.*;
 import com.vivarium.view.SidePanel;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class SidePanelActionListener implements ActionListener {
 
     private SidePanel sidePanel;
     private VivariumController vc;
+    private String organismNameComboBox = null;
 
     public SidePanelActionListener(SidePanel sidePanel, VivariumController vc)
     {
@@ -21,9 +24,29 @@ public class SidePanelActionListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        synchronized(vc) {
-            vc.add(new Wolf(100, 100, vc.getVivarium(), Sex.Male));
-            System.out.println("UN LOUP SAUVAGE APPARAIT");
+        if("Choice".equals(e.getActionCommand())) {
+            JComboBox cb = (JComboBox)e.getSource();
+            organismNameComboBox = (String)cb.getSelectedItem();
+        }
+        System.out.println(organismNameComboBox);
+        if("Spawn".equals(e.getActionCommand())) {
+            synchronized(vc) {
+                try {
+                    Class organism = Class.forName("com.vivarium.model."+organismNameComboBox);
+                    Constructor constructor = organism.getConstructor(int.class,int.class,Vivarium.class,Sex.class);
+                    vc.add((Organism)constructor.newInstance(new Object[]{100,100,vc.getVivarium(),Sex.Male}));
+                } catch (ClassNotFoundException e1) {
+                    e1.printStackTrace();
+                } catch (NoSuchMethodException e1) {
+                    e1.printStackTrace();
+                } catch (IllegalAccessException e1) {
+                    e1.printStackTrace();
+                } catch (InstantiationException e1) {
+                    e1.printStackTrace();
+                } catch (InvocationTargetException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/com/vivarium/view/SidePanel.java
+++ src/com/vivarium/view/SidePanel.java
@@ -7,17 +7,27 @@ import javax.swing.*;
 
 public class SidePanel extends JPanel{
 
-    private JButton wolfButton;
+    String[] organismStrings = {"Bear","Blowfish","Bouquetin","Cactus","Camel","Cow","Dog","Dragon","Eagle","Fish","FreshwaterFish","Grass","Mushroom","Rabbit","Tree","Trex","Wolf"};
     private SidePanelActionListener listener;
 
     public SidePanel(VivariumController vc) {
         super();
         listener = new SidePanelActionListener(this, vc);
-        wolfButton = new JButton("Spawn wolf (TEST)");
-        wolfButton.setVerticalTextPosition(AbstractButton.CENTER);
-        wolfButton.setHorizontalTextPosition(AbstractButton.CENTER);
-        wolfButton.addActionListener(listener);
-        add(wolfButton);
+
+        /* Create ComboBox to select organism to spawn */
+        JComboBox organismList = new JComboBox(organismStrings);
+        organismList.setSelectedIndex(0);
+        organismList.setActionCommand("Choice");
+        organismList.addActionListener(listener);
+        add(organismList);
+
+        /* Create spawn button */
+        JButton spawnButton = new JButton("Spawn");
+        spawnButton.setVerticalTextPosition(AbstractButton.CENTER);
+        spawnButton.setHorizontalTextPosition(AbstractButton.CENTER);
+        spawnButton.setActionCommand("Spawn");
+        spawnButton.addActionListener(listener);
+        add(spawnButton);
     }
 
 }
