0adbe56275eeb6d531a6f6fbaf2eb571a58c0ae0
Piryus <piryus@gmail.com>
2017-12-21 13:04:26 +0100

    Fix last merge conflict (my bad :()

--- src/com/vivarium/view/SidePanel.java
+++ src/com/vivarium/view/SidePanel.java
@@ -7,11 +7,6 @@ import javax.swing.*;
 
 public class SidePanel extends JPanel{
 
-<<<<<<< HEAD
-=======
-    private String[] animalStrings = {"Bear","Blowfish","Bouquetin","Camel","Cow","Dog","Dragon","Eagle","Fish","FreshwaterFish","Rabbit","Trex","Wolf"};
-    private String[] animalSex = {"Male","Female"};
->>>>>>> 3f582f1b526d65680ac5cdb7680e3dbee70f5328
     private SidePanelActionListener listener;
 
     public SidePanel(VivariumController vc) {
@@ -26,34 +21,22 @@ public class SidePanel extends JPanel{
         listener = new SidePanelActionListener(this, vc);
 
         // Create ComboBox to select organism to spawn
-<<<<<<< HEAD
         JLabel organismListLabel = new JLabel("Organism :",JLabel.LEADING);
         add(organismListLabel);
-        String[] organismStrings = {"Bear","Blowfish","Bouquetin","Cactus","Camel","Cow","Dog","Dragon","Eagle","Fish","FreshwaterFish","Grass","Mushroom","Rabbit","Tree","Trex","Wolf"};
-        JComboBox organismList = new JComboBox(organismStrings);
-=======
+        String[] animalStrings = {"Bear","Blowfish","Bouquetin","Cactus","Camel","Cow","Dog","Dragon","Eagle","Fish","FreshwaterFish","Grass","Mushroom","Rabbit","Tree","Trex","Wolf"};
         JComboBox organismList = new JComboBox(animalStrings);
->>>>>>> 3f582f1b526d65680ac5cdb7680e3dbee70f5328
         organismList.setSelectedIndex(0);
         organismList.setActionCommand("Choice");
         organismList.addActionListener(listener);
         add(organismList);
 
         // Create ComboBox to select sex
-<<<<<<< HEAD
-        String[] organismSex = {"Male","Female"};
-        JComboBox organismSexList = new JComboBox(organismSex);
-        organismSexList.setSelectedIndex(0);
-        organismSexList.setActionCommand("Sex");
-        organismSexList.addActionListener(listener);
-        add(organismSexList);
-=======
+        String[] animalSex = {"Male","Female"};
         JComboBox animalSexList = new JComboBox(animalSex);
         animalSexList.setSelectedIndex(0);
         animalSexList.setActionCommand("Sex");
         animalSexList.addActionListener(listener);
         add(animalSexList);
->>>>>>> 3f582f1b526d65680ac5cdb7680e3dbee70f5328
 
         /*// Create text field for organism's name
         JLabel nameTextFieldLabel = new JLabel("Name :",JLabel.LEADING);
