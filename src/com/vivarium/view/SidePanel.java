46130e8fc53b92959424f3330611e972a744c7d4
Piryus <piryus@gmail.com>
2017-12-21 12:48:23 +0100

    Reorganize and comment SidePanel.java

--- src/com/vivarium/view/SidePanel.java
+++ src/com/vivarium/view/SidePanel.java
@@ -7,15 +7,23 @@ import javax.swing.*;
 
 public class SidePanel extends JPanel{
 
-    String[] organismStrings = {"Bear","Blowfish","Bouquetin","Cactus","Camel","Cow","Dog","Dragon","Eagle","Fish","FreshwaterFish","Grass","Mushroom","Rabbit","Tree","Trex","Wolf"};
-    String[] organismSex = {"Male","Female"};
     private SidePanelActionListener listener;
 
     public SidePanel(VivariumController vc) {
         super();
+
+        //Todo Create multiple panels and use holderPanel.setBorder(new EmptyBorder(80, 50, 20, 130)) (https://stackoverflow.com/questions/14550255/jbuttons-inside-jpanels-fill-up-the-whole-panel)
+        // Set the layout of the panel and constraints
+        /*setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();*/
+
+        // Create Listener for ActionEvents
         listener = new SidePanelActionListener(this, vc);
 
         // Create ComboBox to select organism to spawn
+        JLabel organismListLabel = new JLabel("Organism :",JLabel.LEADING);
+        add(organismListLabel);
+        String[] organismStrings = {"Bear","Blowfish","Bouquetin","Cactus","Camel","Cow","Dog","Dragon","Eagle","Fish","FreshwaterFish","Grass","Mushroom","Rabbit","Tree","Trex","Wolf"};
         JComboBox organismList = new JComboBox(organismStrings);
         organismList.setSelectedIndex(0);
         organismList.setActionCommand("Choice");
@@ -23,11 +31,19 @@ public class SidePanel extends JPanel{
         add(organismList);
 
         // Create ComboBox to select sex
+        String[] organismSex = {"Male","Female"};
         JComboBox organismSexList = new JComboBox(organismSex);
         organismSexList.setSelectedIndex(0);
         organismSexList.setActionCommand("Sex");
         organismSexList.addActionListener(listener);
         add(organismSexList);
+
+        /*// Create text field for organism's name
+        JLabel nameTextFieldLabel = new JLabel("Name :",JLabel.LEADING);
+        add(nameTextFieldLabel);
+        JTextField nameTextField = new JTextField(30);
+        nameTextField.addActionListener(listener);
+        add(nameTextField);*/
 
         // Create spawn button
         JButton spawnButton = new JButton("Spawn");
