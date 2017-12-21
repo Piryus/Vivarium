69458186e4488ea7a68a5a198599edab04fd5da5
Piryus <piryus@gmail.com>
2017-12-21 13:39:55 +0100

    Comment AreaType.java

--- src/com/vivarium/model/AreaType.java
+++ src/com/vivarium/model/AreaType.java
@@ -1,13 +1,16 @@
 package com.vivarium.model;
-/**
- * 
- */
+
 public enum AreaType {
     Desert,
     Plain,
     Water,
     Mountain;
 
+    /**
+     * Return a random AreaType, used for terrain generation
+     * @see Terrain#generateAreas()
+     * @return a random AreaType, using Math.random()
+     */
     public static AreaType getRandom() {
         return values()[(int) (Math.random() * values().length)];
     }
