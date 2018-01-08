9a2c7b1bb3f3a4004d49737327c5944dd9e8096c
Piryus <piryus@gmail.com>
2018-01-08 17:57:23 +0100

    Comment for better understanding

--- src/com/vivarium/view/OrganismView.java
+++ src/com/vivarium/view/OrganismView.java
@@ -11,23 +11,36 @@ import java.io.IOException;
 
 public class OrganismView {
 
-    private BufferedImage icon; // TODO Load only one images for each organism
-    private final int iconSize = 50;
-    private Organism organism;
+    private BufferedImage icon; // An image used in-game to display the organism
+    private final int iconSize = 50; // Might be dynamic later
+    private Organism organism; // The organism the view will display
 
+    /**
+     * Constructor, calls #assignIcon()
+     * @param organism the organism to create the view of
+     * @see #assignIcon()
+     */
     public OrganismView(Organism organism) {
         this.organism = organism;
         assignIcon();
     }
 
+    /**
+     * Assign an icon to the OrganismView, this icon is used in-game to display the organism
+     * Every organism's icon is stored in resources/organisms/
+     */
     private void assignIcon() {
         try {
-            icon = ImageIO.read(new File("resources/organisms/"+organism.getClass().getSimpleName()+".png")); // TODO Sex type
+            icon = ImageIO.read(new File("resources/organisms/"+organism.getClass().getSimpleName()+".png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Simply display the organism in-game
+     * @param g the Graphics object, used to draw in the panel
+     */
     public void draw(Graphics g) {
         if (organism instanceof Vegetal){
             if (((Vegetal) organism).getEdible()){
@@ -40,6 +53,10 @@ public class OrganismView {
 
     }
 
+    /**
+     * A simple getter for the organism
+     * @return the organism used by the OrganismView
+     */
     public Organism getOrganism() {
         return organism;
     }
