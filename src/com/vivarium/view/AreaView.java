95b320733d0459696c56719d14c438b77157a985
Piryus <piryus@gmail.com>
2018-01-08 18:11:44 +0100

    Comment functions

--- src/com/vivarium/view/AreaView.java
+++ src/com/vivarium/view/AreaView.java
@@ -11,20 +11,30 @@ import java.io.IOException;
 
 public class AreaView {
 
-    private BufferedImage texture;
-    private Area area;
+    private BufferedImage texture; // The area's texture displayed in game (sand, grass, rocks, ...)
+    private Area area; // The Area represented by the AreaView
 
+    /**
+     * Constructor, calls #assignTexture
+     * @param area the Area to display
+     * @see #assignTexture(AreaType)
+     */
     public AreaView(Area area) {
         this.area = area;
         assignTexture(this.area.getAreaType());
     }
 
+    /**
+     * Draw the area's texture to its coordinates
+     * @param g
+     */
     public void draw(Graphics g) {
         g.drawImage(this.texture, (int)area.getCoords().getX(), (int)this.area.getCoords().getY(), null);
     }
 
     /**
-     * Assign texture to this (Area)
+     * Assign a texture to an area depending on its type
+     * @param type the area's type (desert, plains, mountain, ...), used to determine which texture to use
      */
     private void assignTexture(AreaType type) {
         try {
